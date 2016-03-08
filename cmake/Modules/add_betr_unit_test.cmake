1f9522972e3e5de3626abf17f43627b2f982cd1a
Ben Andre <andre@ucar.edu>
2016-03-08 12:47:44 -0700

    Working pfunit test with make test
    First working pfunit test runs with 'make test'. The cmake
    add_betr_unit_test function needs to be generalized a bit because it
    hard codes include paths.
    
    Testing: configure, build, test work on os x. Manually run mock problem
    to completion without error.

--- cmake/Modules/add_betr_unit_test.cmake
+++ cmake/Modules/add_betr_unit_test.cmake
@@ -1,7 +1,12 @@
-# This function adds a unit test executable for Betr.
+# This function adds a pfunit test executable for BeTR.
 function(add_betr_unit_test exe)
-  add_executable(${exe} ${ARGN})
-  target_link_libraries(${exe} betr;${BETR_TPLS})
+  configure_file(${CMAKE_BINARY_DIR}/include/driver.F90 driver.F90 COPYONLY)
+  add_executable(${exe} ${ARGN} driver.F90)
+  target_link_libraries(${exe} pfunit;${BETR_LIBRARIES})
+  include_directories(${CMAKE_BINARY_DIR}/mod)
+  include_directories(${CMAKE_BINARY_DIR}/src/shr)
+  include_directories(${CMAKE_BINARY_DIR}/src/betr/betr_math)
+  set_target_properties(${exe} PROPERTIES LINKER_LANGUAGE Fortran)
   set_target_properties(${exe} PROPERTIES COMPILE_FLAGS "-DCMAKE_CURRENT_SOURCE_DIR=\\\"${CMAKE_CURRENT_SOURCE_DIR}\\\"")
   add_test(${exe} ${exe})
   set_tests_properties(${exe} PROPERTIES WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
