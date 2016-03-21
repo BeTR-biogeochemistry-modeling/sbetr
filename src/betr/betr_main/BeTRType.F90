4d78900ac815a88cf2198a0a9f6fb630ad696c7f
add new files <jinyuntang@lbl.gov>
2016-03-21 14:26:41 -0700

    add changes lost in last commit

--- src/betr/betr_main/BeTRType.F90
+++ src/betr/betr_main/BeTRType.F90
@@ -360,7 +360,7 @@ contains
     integer,                  intent(in)    :: filter_soilc(:)                    ! column filter_soilc
     integer,                  intent(in)    :: jtops(bounds%begc: )
     type(betr_waterflux_type)    , intent(in)    :: waterflux_vars
-  
+
     ! !LOCAL VARIABLES:
     real(r8) :: aqucon
     integer  :: fc, c, j, k
@@ -640,8 +640,6 @@ contains
 
    end associate
    end subroutine diagnose_dtracer_freeze_thaw
-
-
 
   !------------------------------------------------------------------------
   subroutine betr_lsm_flux_state_sendback(this, bounds,  num_soilc, filter_soilc )
