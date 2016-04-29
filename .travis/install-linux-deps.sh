a9339547897a29af9e6cf1ebaa976ca98d507afa
Ben Andre <andre@ucar.edu>
2016-04-29 17:46:52 -0600

    Add files for travis-ci testing.
    Testing:
        configure and build - os x, gfortran 5.3
        unit tests - pass
        regression tests - pass
        meta tests - pass

---
+++ .travis/install-linux-deps.sh
@@ -0,0 +1,3 @@
+sudo apt-get update -qq
+sudo apt-get install -y cmake gcc liblapack-dev gfortran git netcdf-bin libnetcdf-dev 
+
---
+++ .travis/install-osx-deps.sh
@@ -0,0 +1,10 @@
+# Install required software
+brew update
+brew install git
+brew upgrade cmake
+brew tap homebrew/science
+brew install netcdf --with-fortran 
+
+# Make sure the weird gfortran library links are in place.
+ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
+ln -s /usr/local/lib/gcc/5/libgfortran.a /usr/local/lib/libgfortran.a
