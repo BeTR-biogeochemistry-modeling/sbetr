41c27f941effc9127c68ef6492c86f13822142b5
Ben Andre <andre@ucar.edu>
2016-04-30 11:02:41 -0600

    Update fortran version installed by travis-ci instances.
    Testing:
        none - no source or config changes

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -1,3 +1,4 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
-sudo apt-get install -y cmake gcc liblapack-dev gfortran git netcdf-bin libnetcdf-dev 
+sudo apt-get install -y cmake gcc-5 liblapack-dev gfortran-5 git netcdf-bin libnetcdf-dev
 
--- .travis/install-osx-deps.sh
+++ .travis/install-osx-deps.sh
@@ -3,7 +3,7 @@ brew update
 brew install git
 brew upgrade cmake
 brew tap homebrew/science
-brew install netcdf --with-fortran 
+brew install gcc netcdf --with-fortran
 
 # Make sure the weird gfortran library links are in place.
 ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
