7b73379235506eb1a95d06f705d80a4d55e2847c
Ben Andre <andre@ucar.edu>
2016-04-30 11:43:22 -0600

    travis mac build update.
    Remove the netcdf c++ interface from the mac build because we don't need
    it and it saves us a minute of build time.
    
    Testing:
        none - travis up date only, no code or cmake changes.

--- .travis/install-osx-deps.sh
+++ .travis/install-osx-deps.sh
@@ -4,7 +4,7 @@ brew install git
 brew upgrade cmake
 brew tap homebrew/science
 brew unlink gcc
-brew install gcc netcdf --with-fortran
+brew install gcc netcdf --with-fortran --without-cxx
 
 # Make sure the weird gfortran library links are in place.
 ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
