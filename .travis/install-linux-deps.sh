464e0c341773bbb546773b6013fe63b693fb63bf
Ben Andre <andre@ucar.edu>
2016-05-02 09:25:21 -0600

    Debug travis-ci linux build.
    Try to determine the name of the gfortran 5 executable on travis-ci
    linux build.
    
    Testing:
        none - travis-ci change only.

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -2,3 +2,4 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get install -y cmake gcc-5 liblapack-dev gfortran-5 git netcdf-bin libnetcdf-dev
 
+ls -al /usr/bin/gfor*
