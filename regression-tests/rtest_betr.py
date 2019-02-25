d6ba6c3772f316455bffe1e02ba2f63d114913ff
Jinyun Tang <jinyuntang@gmail.com>
2019-02-25 13:40:16 -0800

    update regression test to python3
    This fixed some outdated feature of python2. All
    tests passed.

--- regression-tests/rtest_betr.py
+++ regression-tests/rtest_betr.py
@@ -1079,10 +1079,10 @@ def main(options):
         raise RuntimeError(msg)
 
     dry_run = options.dry_run
-    import commands
+    import subprocess
     #ncgen = find_executable('ncgen', exe_paths)
-    loc=commands.getstatusoutput('which ncgen')
-    ncgen=loc[1]
+    loc=subprocess.check_output(["which", "ncgen"])
+    ncgen=print(loc.strip().decode())
     convert_input_data(ncgen, input_dir, dry_run)
 
     print('Setting up tests.')
